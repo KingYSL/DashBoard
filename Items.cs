98b54f6ae806f469201bd32e1c8c0e9d2e6c06d2
KingYSL <rhozando@hotmail.com>
2021-07-14 13:05:49 -0400

    Add files via upload
    This is a simple charting program for Binance.com.
    There are two versions. One uses OxyPlot and the other Microsoft charting library 
    You can change the symbol where "candles" is initialized 
    the timeframes and everything else are automatically set to get the maximum amount of candles from whenever you run it

---
+++ Items.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using Binance.Net;
+using Binance.Net.Objects.Spot;
+using CryptoExchange.Net.Authentication;
+using DashBoard;
+using OxyPlot;
+namespace DashBoard{
+    class Items
+    {
+        public int ID { set; get; }
+
+        public string Text { set; get; }
+
+
+        public override string ToString()
+        {
+            return Text;
+        }
+
+    }
+    
+    
+}
+
---
+++ User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashBoard
+{
+    class User
+    {
+        public string user="Admin";
+        public string Password="Admin";
+
+        public User (string UserId, string Pswrd)
+        {
+            
+        }
+    }
+}
